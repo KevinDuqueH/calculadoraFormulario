fedaad6b866355382347b21676778f4a793a974f
KevinDuqueH <kevin-kad-14@hotmail.com>
2020-08-30 10:17:37 -0500

    Formulario listo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,51 @@
-const inputName= document.getElementById("InputName")
-const nombreMensaje=document.getElementById("nombreMensaje")
+const inputName = document.getElementById("inputName")
+const nombreMensaje = document.getElementById("nombreMensaje")
+const nombreApellido = document.getElementById("nombreApellido")
+const nombrePassword = document.getElementById("nombrePassword")
+const nombreCorreo = document.getElementById("nombreCorreo")
+const nombreCelular = document.getElementById("nombreCelular")
 console.log(inputName)
+console.log(nombreMensaje)
 
 function validarFormulario(event) {
     event.preventDefault()
    if (inputName.value == "") {
-    swal("Good job!", "You clicked the button!", "error");
-       inputName.classList.add("alert");
-       nombreMensaje.innereText = "Llene este campo por favor"
-       inputName.placeholder = "Pepito Perez"
-   } 
-
+    swal("Error!", "Por favor llene todos los campos!", "error");
+       inputName.classList.add('alert');
+       nombreMensaje.innerText = 'Llene este campo del nombre por favor'
+       inputName.placeholder = 'Pepito Perez'
+   }
+   if (inputName.value == "") {
+    swal("Error!", "Por favor llene todos los campos!", "error");
+       inputName.classList.add('alert');
+       nombreApellido.innerText = 'Llene este campo del apellido por favor'
+       inputName.placeholder = 'Pepito Perez'
+   }
+   if (inputName.value == "") {
+    swal("Error!", "Por favor llene todos los campos!", "error");
+       inputName.classList.add('alert');
+       nombrePassword.innerText = 'Llene este campo del password por favor'
+       inputName.placeholder = 'Pepito Perez'
+   }
+   if (inputName.value == "") {
+    swal("Error!", "Por favor llene todos los campos!", "error");
+       inputName.classList.add('alert');
+       nombreCorreo.innerText = 'Llene este campo del correo por favor'
+       inputName.placeholder = 'Pepito Perez'
+   }  
+   if (inputName.value == "") {
+    swal("Error!", "Por favor llene todos los campos!", "error");
+       inputName.classList.add('alert');
+       nombreCelular.innerText = 'Llene este campo del celular por favor'
+       inputName.placeholder = 'Pepito Perez'
+   }
    if (inputName.value !== ""){
-    swal("Good job!", "You clicked the button!", "success");
+    swal("Buen Trabajo!", "Bienvenido a la calculadora", "success");
     setTimeout (()=>{
         window.location ="./calculadora.html"
-
     }, 4000)
-
    }
+   
 }
 
 /* Calculadora*/
